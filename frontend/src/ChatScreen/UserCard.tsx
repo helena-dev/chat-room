dd8a9ef2b77d8690ee61eaaafa2b76504c91abbd
Helena Recasens <helena.recasensg@gmail.com>
2022-02-22 15:03:23 +0100

    moved location and date formatting to utils module

--- frontend/src/ChatScreen/UserCard.tsx
+++ frontend/src/ChatScreen/UserCard.tsx
@@ -1,8 +1,5 @@
-import React, { ReactNode } from "react"
-import { formatDate } from "../utils"
-import { exceptionalReservationsToISO, isoAlpha2ToSymbols } from "../geo"
-import Icon from "@mdi/react"
-import { mdiHome } from "@mdi/js"
+import React from "react"
+import { formatRelativeDate, formatUserLocation } from "../utils"
 import { UserInfo } from "../../../messages"
 import "./UserCard.css"
 
@@ -15,32 +12,11 @@ export default class UserCard extends React.Component<UserCardProps> {
     render() {
         const {user, typingStatus} = this.props
 
-        function formatUserLocation(region?: string, countryCode?: string, bogon?: boolean, city?: string): ReactNode {
-            if (bogon) {
-                return (
-                    <>
-                        <Icon path={mdiHome} size={"1em"} /><span> Local</span>
-                    </>
-                )
-            } else if (countryCode) {
-                const ISO = (region && (region in exceptionalReservationsToISO)) ? exceptionalReservationsToISO[region] : countryCode
-                const symbols = isoAlpha2ToSymbols(ISO)
-                return city ? `${symbols}, ${city}` : `${symbols}`
-            } else {
-                return "🏴‍☠️"
-            }
-        }
-
         const { region, countryCode, bogon, city } = user.ipInfo || {}
         const lastActivityDate = new Date(user.lastActivity)
         const onlineStatus = user.online
         const currentDate = new Date()
-        const userDate = lastActivityDate.getFullYear() !== currentDate.getFullYear() ?
-            lastActivityDate.toLocaleDateString("en-IE", { month: "short", year: "numeric" }) :
-            lastActivityDate.getDay() !== currentDate.getDay() ?
-                lastActivityDate.toLocaleDateString("en-IE", { day: "numeric", month: "short" }) :
-                formatDate(lastActivityDate)
-        const userActivityInfo = [["typing...", "fancyText"], ["online", "fancyText"], ["last seen " + userDate, "plainText"]]
+        const userActivityInfo = [["typing...", "fancyText"], ["online", "fancyText"], ["last seen " + formatRelativeDate(lastActivityDate, currentDate), "plainText"]]
         const position = typingStatus ? 0 : (onlineStatus ? 1 : 2)
         const userActivity = (
             <span className={"user-activity " + userActivityInfo[position][1]}>
---
+++ frontend/src/utils.tsx
@@ -0,0 +1,54 @@
+
+import Icon from "@mdi/react"
+import { mdiHome } from "@mdi/js"
+import { exceptionalReservationsToISO, isoAlpha2ToSymbols } from "./geo"
+import { ReactNode } from "react"
+
+export function randomInt(start: number, end: number): number {
+    return Math.floor(Math.random() * (end + 1 - start) + start)
+}
+
+export function formatDate(preformatDate: Date): string {
+    const formatNum = (x: number) => x.toString().padStart(2, "0")
+    return `${formatNum(preformatDate.getHours())}:${formatNum(preformatDate.getMinutes())}`
+}
+
+export function assertUnreachable(): never {
+    throw Error("Unreachable code. Prolly a bug and stuff.")
+}
+
+export function rgbToHex([r, g, b]: number[]): number {
+    const hex = (r << 16) | (g << 8) | b
+    return hex
+}
+
+export function hexToRgb(hex: number): number[] {
+    const r = hex >> 16
+    const g = (hex >> 8) & 0b11111111
+    const b = hex & 0b11111111
+    return [r,g,b]
+}
+
+export function formatUserLocation(region?: string, countryCode?: string, bogon?: boolean, city?: string): ReactNode {
+    if (bogon) {
+        return (
+            <>
+                <Icon path={mdiHome} size={"1em"} /><span> Local</span>
+            </>
+        )
+    } else if (countryCode) {
+        const ISO = (region && (region in exceptionalReservationsToISO)) ? exceptionalReservationsToISO[region] : countryCode
+        const symbols = isoAlpha2ToSymbols(ISO)
+        return city ? `${symbols}, ${city}` : `${symbols}`
+    } else {
+        return "🏴‍☠️"
+    }
+}
+
+export const formatRelativeDate = (lastActivityDate: Date, currentDate: Date) => (
+    lastActivityDate.getFullYear() !== currentDate.getFullYear() ?
+    lastActivityDate.toLocaleDateString("en-IE", { month: "short", year: "numeric" }) :
+    lastActivityDate.getDay() !== currentDate.getDay() ?
+        lastActivityDate.toLocaleDateString("en-IE", { day: "numeric", month: "short" }) :
+        formatDate(lastActivityDate)
+)
